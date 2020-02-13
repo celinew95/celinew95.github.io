ee2e1e629cc494d493c704b8b5ea223d2426e6d9
celinew95 <60239466+celinew95@users.noreply.github.com>
2020-02-13 14:45:21 -0600

    Add files via upload

--- TomandJerryExercise/sketch.js
+++ TomandJerryExercise/sketch.js
@@ -1,3 +1,4 @@
+var screen = 0;
 let jerry;
 let aImg;
 let fImg;
@@ -7,56 +8,108 @@ let lImg;
 var gif_loadImg, gif_createImg;
 let flowers = [];
 let score = 0;
+let beginScreenBool = true;
+let gameBool = false;
 let endScreenBool = false;
 var mySound;
 
 function preload(){
-aImg = loadImage('image/jerry.png');
-fImg = loadImage('image/bomb.png');
-tImg = loadImage('image/tom.png');
-bImg = loadImage('background.jpg');
-lImg = loadImage('image/logo2.png');
-mySound = loadSound('music.mp3');
-
+  aImg = loadImage('image/jerry.png');
+  fImg = loadImage('image/bomb.png');
+  tImg = loadImage('image/tom.png');
+  bImg = loadImage('background.jpg');
+  lImg = loadImage('image/logo2.png');
+  mySound = loadSound('music.mp3');
 }
+
 function setup() {
   createCanvas(windowWidth, 450);
   jerry = new Jerry();
-  // mySound.playMode('restart');
   mySound.setVolume(0.3);
   mySound.play();
-
+  gif_createImg = createImg("image/tomkissesjerry.gif");
+  gif_createImg.hide();
 }
 
 function keyPressed(){
   if(key == " "){
     jerry.jump();
-
   }
 }
 
 
 function draw() {
-mySound.playMode('restart');
-  if(flowers.length == 0){
-  // if(random(1)<0.007){
-    flowers.push(new Flower());
-  // mySound.play();
+  if(beginScreenBool == true){
+    beginScreen();
+  }
+
+  if(gameBool == true){
+    game();
+
+  }
+
+  if(endScreenBool == true){
+    endScreen();
+    gif_createImg.show();
+  }
+
 }
 
+function mousePressed(){
+  //  mySound.play();
+  // if (mySound.isPlaying()) {
+  //   // .isPlaying() returns a boolean
+  //   mySound.stop();
+  //
+  // } else {
+  //   mySound.play();
+  //   print("play audio");
+  // }
+  if(beginScreenBool == true){
+
+    gameBool = true;
+    if(gameBool = true){
+       mySound.play();
+    }
+    beginScreenBool = false;
+  }
+
+  if(endScreenBool == true){
+    score = 0;
+    gif_createImg.hide();
+    endScreenBool = false
+    gameBool = false;
+    beginScreenBool = true;
+
+  }
+
+}
+function beginScreen(){
+  background(bImg)
+  strokeWeight(2);
+  fill(245, 200, 66)
+  textSize(35);
+  textAlign(CENTER);
+  text('Click to Start', width/2, height/2);
+}
+
+function game(){
+  if(flowers.length == 0){
+    // if(random(1)<0.007){
+    flowers.push(new Flower());
+     //mySound.play();
+  }
 
   background(bImg);
   image(tImg, windowWidth - 200, height - 250, 250, 250);
   image(lImg,10, 10, 80, 80);
   fill(255);
   textSize(20);
-  text(CENTER);
+  textAlign(CENTER);
   text("Score" + score + "00", width/2, 50);
-
 
   jerry.show();
   jerry.move();
-
 
   for(let f of flowers){
     f.move();
@@ -64,28 +117,22 @@ mySound.playMode('restart');
     if(jerry.hits(f)){
       endScreenBool = true;
       console.log('game over');
-      noLoop();
+      //noLoop();
     }
 
     if(f.x < 0 ){
       score++;
       flowers.splice(f, 1);
     }
-    print(flowers.length);
   }
-
-  if(endScreenBool == true){
-    endScreen();
-  }
-
 
 }
-
+//game over scene
 function endScreen(){
   background(245, 200, 66);
-  gif_loadImg = loadImage("image/tomkissesjerry.gif");
-  gif_createImg = createImg("image/tomkissesjerry.gif");
-  image(gif_loadImg, 50, 50);
+  //gif_loadImg = loadImage("image/tomkissesjerry.gif");
+
+  //image(gif_loadImg, 50, 50);
   gif_createImg.position(width/2 - 120, height/2 + 20);
   stroke(0, 58, 139);
   strokeWeight(3);
